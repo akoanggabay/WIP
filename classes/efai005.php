84ebd6fd9298c00ffa82ff6de995d70cc453bd06
unknown <duanealpuerto@gmail.com>
2022-09-19 14:08:29 +0800

    MES UAT BG

--- classes/efai005.php
+++ classes/efai005.php
@@ -147,7 +147,7 @@ class efai005 {
 		return $this->active;
 	}
 
-    public function AddeFAI0052(){
+    public function AddeFAI005(){
 		$conn = new Connection();
         $success = true;
 		try{
@@ -156,32 +156,6 @@ class efai005 {
 			$con = $conn->open();
             $sql = "INSERT INTO dbo.efai005 (custcode,intlot,wswr,swrno,wafertype,highmaginspection,wafernoinspected,remarks,lastupdate,lastupdatedby,active,wafersize) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
             $params = array($this->getcustcode(),$this->getintlot(),$this->getwswr(),$this->getswrno(),$this->getwafertype(),$this->gethighmaginspection(),$this->getwafernoinspected(),$this->getremarks(),date("Y-m-d h:i:s"),$this->getlastupdatedby(),1,$this->getwafersize());
-            $stmt = sqlsrv_query( $con, $sql, $params);
-            $row = sqlsrv_rows_affected($stmt);
-            if($row == true)
-            {
-                $success = true;
-            }
-            else
-            {
-                $success = false;
-            }
-			//$conn->close();
-		}catch(Exception $e){
-            $success = false;
-		}
-        return $success;	
-	}
-
-    public function AddeFAI005(){
-		$conn = new Connection();
-        $success = true;
-		try{
-			//$conn->open();
-			//$result = $conn->query("INSERT INTO dbo.PO (pono,custcode,qty,processcat,subprocesscat,status,lastupdate,lastupdatedby,active) VALUES('".$this->getpono()."','".$this->getcustcode()."','".$this->getqty()."','".$this->getprocesscat()."','".$this->getsubprocesscat()."','".$this->getstatus()."',NOW(),'".$this->getlastupdatedby()."',1)");
-			$con = $conn->open();
-            $sql = "INSERT INTO dbo.efai005 (custcode,intlot,wswr,swrno,wafertype,highmaginspection,wafernoinspected,remarks,lastupdate,lastupdatedby,active) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
-            $params = array($this->getcustcode(),$this->getintlot(),$this->getwswr(),$this->getswrno(),$this->getwafertype(),$this->gethighmaginspection(),$this->getwafernoinspected(),$this->getremarks(),date("Y-m-d h:i:s"),$this->getlastupdatedby(),1);
             $stmt = sqlsrv_query( $con, $sql, $params);
             $row = sqlsrv_rows_affected($stmt);
             if($row == true)
