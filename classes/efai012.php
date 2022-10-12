d45999dc9f8b3f9fe807ee06c87a486efd4466fc
unknown <duanealpuerto@gmail.com>
2022-10-12 15:23:48 +0800

    Parts report alert

--- classes/efai012.php
+++ classes/efai012.php
@@ -200,7 +200,7 @@ class efai012 {
 			//$result = $conn->query("INSERT INTO dbo.PO (pono,custcode,qty,processcat,subprocesscat,status,lastupdate,lastupdatedby,active) VALUES('".$this->getpono()."','".$this->getcustcode()."','".$this->getqty()."','".$this->getprocesscat()."','".$this->getsubprocesscat()."','".$this->getstatus()."',NOW(),'".$this->getlastupdatedby()."',1)");
 			$con = $conn->open();
             $sql = "INSERT INTO dbo.efai012 (custcode,intlot,wswr,swrno,tally,qtyouttally,detailsattached,canisterused,canisternoused,properlyvacuum,totalnoofringsused,remarks,lastupdate,lastupdatedby,active,wafersize) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-            $params = array($this->getcustcode(),$this->getintlot(),$this->getwswr(),$this->getswrno(),$this->gettally(),$this->getqtyouttally(),$this->getdetailsattached(),$this->getcanisterused(),$this->getcanisternoused(),$this->getproperlyvacuum(),$this->gettotalnoofringsused(),$this->getremarks(),date("Y-m-d h:i:s"),$this->getlastupdatedby(),1,$this->getwafersize());
+            $params = array($this->getcustcode(),$this->getintlot(),$this->getwswr(),$this->getswrno(),$this->gettally(),$this->getqtyouttally(),$this->getdetailsattached(),$this->getcanisterused(),$this->getcanisternoused(),$this->getproperlyvacuum(),$this->gettotalnoofringsused(),$this->getremarks(),date("Y-m-d H:i:s"),$this->getlastupdatedby(),1,$this->getwafersize());
             $stmt = sqlsrv_query( $con, $sql, $params);
             $row = sqlsrv_rows_affected($stmt);
             if($row == true)
